fd4e90d074a1a88c8d9ca37a773f07c5cdc511a6
ira47 <1196951630@qq.com>
2020-03-01 19:35:10 +0800

    列出101验证码问题

--- writedown.py
+++ writedown.py
@@ -1,29 +1,32 @@
-#-*- coding: UTF-8 -*-
- 
-import sys  
+# -*- coding: UTF-8 -*-
+
+import sys
 import json
-reload(sys)   
-sys.setdefaultencoding('utf8') 
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+
 class Wd():
     def __init__(self, temporaryDataFileName, DataFileName):
         self.tdfn = temporaryDataFileName
         self.dfn = DataFileName
     # 记录临时数据
-    def wd_temporaryData(self,jsonData):
+
+    def wd_temporaryData(self, jsonData):
         # pass
-        file_object = open(self.tdfn,'a')
+        file_object = open('temp.csv', 'a')
         for d in jsonData['msg'][1]:
-            file_object.write(json.dumps(d[0][1],ensure_ascii=False, encoding="utf-8"))
+            file_object.write(json.dumps(
+                d[0][1], ensure_ascii=False, encoding="utf-8"))
             file_object.write(',')
-            file_object.write(json.dumps(d[0][2],ensure_ascii=False, encoding="utf-8"))
+            file_object.write(json.dumps(
+                d[0][2], ensure_ascii=False, encoding="utf-8"))
             file_object.write('\n')
         file_object.close()
     # 记录话题数据
-    def wd_Data(self,title,id,focus,parentsname,parentsid):
-        file_object = open(self.dfn,'a')
-        file_object.write('\n'+str(title)+','+str(id)+','+str(focus)+','+str(parentsname)+','+str(parentsid))
+
+    def wd_Data(self, title, id, focus, parentsname, parentsid):
+        file_object = open('data.csv', 'a')
+        file_object.write('\n'+str(title)+','+str(id)+',' +
+                          str(focus)+','+str(parentsname)+','+str(parentsid))
         file_object.close()
-
-
-
-          
