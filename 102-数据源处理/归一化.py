8b039394bd95603fff8e8a919cf99b68b302df37
ira47 <1196951630@qq.com>
2020-03-09 17:16:18 +0800

    加了README 改了102topic数据生成规则

--- 102-数据源处理/归一化.py
+++ 102-数据源处理/归一化.py
@@ -2,6 +2,9 @@ import csv
 
 index = 0
 topic_list = []
+html_head = 'https://www.zhihu.com/topic/'
+html_tail = '/hot'
+
 read = open('Data_sort.csv', 'r', newline='')
 reader = csv.reader(read)
 write = open('topic_final.csv', 'w', newline='')
@@ -10,9 +13,10 @@ for row in reader:
     topic_id = row[1]
     topic_name = row[0]
     topic_viewer = row[2]
+    url = html_head + topic_id + html_tail
 
     if topic_id not in topic_list:
-        writer.writerow([topic_id, topic_name, topic_viewer])
+        writer.writerow([topic_id, topic_name, topic_viewer, url])
         topic_list.append(topic_id)
 
     index += 1
